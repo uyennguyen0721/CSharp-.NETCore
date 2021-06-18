36c48ad04a962652db17287c9be2dd198f767a17
Uyen Nguyen <1851050182uyen@ou.edu.vn>
2021-06-18 22:33:02 +0700

    upload ADO.NET Project

---
+++ ADO.NET/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sqlStringConn = "Data Source = localhost,80; Initial Catalog = xtlab; User ID = sa; Password = 123456789";  // chuối kết nối
+            using var conn = new SqlConnection(sqlStringConn);
+            Console.WriteLine(conn.State); //trạng thái của conn
+            conn.Open();
+            Console.WriteLine(conn.State); //trạng thái của conn
+
+        }
+    }
+}

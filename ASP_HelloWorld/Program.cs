fbe29104f5790911a1cd94c657d0ea0616aec4a7
Uyen Nguyen <1851050182uyen@ou.edu.vn>
2021-07-03 22:05:23 +0700

    upload ASP_HelloWorld Project

---
+++ ASP_HelloWorld/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_HelloWorld
+{
+    public class Program
+    {
+        /* Để thiêt lập và khởi chạy một ứng dụng ASP.NET, cơ bản ta phải làm các việc như sau:
+           Tạo ra một đối tượng Host, đối tượng này được triển khai từ interface Host (IHost) và chứa các thành phần:
+                - Các đối tượng liên quan đến kỹ thuật Dependence Injection (DI): điều này có nghĩa là trong đối tượng Host
+        này chứa đối tượng IServiceProvider để lấy ra các dịch vụ (ServiceCollection)
+                - Các đối tượng trong Host chứa các dịch vụ liên quan đến Logging (ILogging)
+                - Configuration: Toàn bộ cấu hình của ứng dụng
+                - Có lớp được triển khai từ IHostedService => đối tượng được triển khai từ IHostedService có phương thức StartAsync,
+        Khi phương thức này khởi chạy, nó sẽ chạy một máy chủ HTTP (HTTP Server) được tích hợp sẵn trong thư viện .NET (máy chủ có tên Kestrel Http).
+        Khi máy chủ trong Host này chạy, thì nó bắt đầu nhận các yêu cầu HTTP từ Client gửi đến, cái máy chủ này chạy và lắng nghe đến khi chúng ta ra 
+        lệnh kết thúc ứng dụng
+        ===> Cấu trúc cơ bản của đối tượng Host trong ứng dụng ASP.NET
+
+        * Để chạy đối tượng Host thì ta cần phải
+            1) Tạo IHostBuilder
+            2) Dùng IHostBuilder để thiết lập các cấu hình, đăng ký các dịch vụ, bằng cách gọi phương thức ConfigureWebHostDefaults của IHostBuilder
+            3) IHostBuilder.Build() => tạo ra đối tượng Host (IHost)
+            4) Host.Run() để chạy Host
+
+        * Trong ứng dụng ASP.NET, khi nhận được các request thì các request đó phải được chuyên xử lý và đi qua các đoạn code và cuối cùng trả về các Respond
+        * Các thành phần mà Resquest phải đi qua được gọi là pipeline (là một chuỗi những cái gọi là Middleware)
+         */
+
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>  // Cấu hình mặc định cho HOST tạo ra
+                {
+                    //Tùy biến thêm về Host
+
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ ASP_HelloWorld/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ASP_HelloWorld
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        // Dùng để đăng ký các dịch vụ của ứng dụng, sử dụng các dịch vụ DI
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Dùng để xây dựng pipeline (những chuỗi Middleware)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}

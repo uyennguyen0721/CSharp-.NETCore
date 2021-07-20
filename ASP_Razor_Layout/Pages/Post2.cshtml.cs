a6d51a8c3bfc638d43188af316b27e88589ef040
Uyen Nguyen <1851050182uyen@ou.edu.vn>
2021-07-20 10:50:38 +0700

    upload ASP_Razor_Layout Project

---
+++ ASP_Razor_Layout/Pages/Post2.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP_Razor_Layout.Pages
+{
+    public class Post2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ASP_Razor_Layout/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Razor_Layout
+{
+    /*
+     Layout là một bố cục của trang (header, footer, menu, content, ...) mà các trang sử dụng chung
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}

1045be2897a349b4dca68f516655ccb5363d96c6
Uyen Nguyen <1851050182uyen@ou.edu.vn>
2021-07-20 21:31:59 +0700

    upload ASP_Razor_ModelBinding

---
+++ ASP_Razor_ModelBinding/Pages/Customer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Razor_ModelBinding.Pages
+{
+    [Bind("Email", "UserName")] // chỉ binding 2 thuộc tính Email và UserName của lớp Customer
+    public class Customer
+    {
+        [BindRequired]
+        public int CustomerID { set; get; }
+        public string Email { set; get; }
+        public string UserName { set; get; }
+    }
+}
---
+++ ASP_Razor_ModelBinding/Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Razor_ModelBinding.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        // Binding Email từ dữ liệu từ nguồn tới có tên Email, email, emaIL ...
+        [BindProperty]
+        public string Email { get; set; }
+
+        // Binding cho UserId từ nguồn gửi đến, dữ liệu nguồn có tên username
+        [BindProperty(Name = "username")]
+        public string UserId { set; get; }
+
+        // Binding ProductID - thiết lập BINDING ngay cả khi truy cập là HTTP GÉT
+        [BindProperty(SupportsGet = true)]
+        public int ProductID { set; get; }
+
+        // Binding Color
+        [BindProperty]
+        public string Color { set; get; }
+
+        [BindProperty(SupportsGet = true)]
+        public Customer customer { set; get; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet(int? productID, string color)
+        {
+            Console.WriteLine($"ProductID: {productID}; color: {color}");
+        }
+        // Handler gọi khi truy vấn bằng HTTP POST
+        public void OnPost()
+        {
+            // Microsoft.AspNetCore.Http.Extensions -> GetDisplayUrl
+            Console.WriteLine(Request.GetDisplayUrl());
+            var req = Request;
+        }
+    }
+}

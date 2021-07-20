3e2219820383ff572123321d2fc169feb87740f6
Uyen Nguyen <1851050182uyen@ou.edu.vn>
2021-07-20 20:10:39 +0700

    upload ASP_Razor_TagHelper Project

---
+++ ASP_Razor_TagHelper/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Razor_TagHelper.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [DisplayName("Tên người dùng")]
+        public string Username { set; get; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ ASP_Razor_TagHelper/TagHelper/MyListTaghelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ASP_Razor_TagHelper.TagHelper 
+{
+
+    // thẻ sẽ là mylist
+    [HtmlTargetElement("mylist")]
+    public class MyListTagHelper : ITagHelper
+    {
+        public int Order => throw new NotImplementedException();
+
+        // Thuộc tính sẽ là list-title
+        public string ListTitle { get; set; }
+
+        // Thuộc tính sẽ là list-items
+        public List<string> ListItems { set; get; }
+
+        public void Init(TagHelperContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "ul";    // ul sẽ thay cho myul
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.Attributes.SetAttribute("class", "list-group");
+            output.PreElement.AppendHtml($"<h2>{ListTitle}</h2>");
+
+
+            StringBuilder content = new StringBuilder();
+            foreach (var item in ListItems)
+            {
+                content.Append($@"<li class=""list-group-item"">{item}</li>");
+            }
+            output.Content.SetHtmlContent(content.ToString());
+        }
+    }
+
+}
